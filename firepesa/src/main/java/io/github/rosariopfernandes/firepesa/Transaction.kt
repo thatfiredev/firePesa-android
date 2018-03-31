36c2f850df93135d1b714a97fa51d5b2057253be
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-03-31 16:57:45 +0200

    Added LICENSE and Docs Added the MIT LICENSE File and copied it to every class Added javadoc to every class and methods

--- firepesa/src/main/java/io/github/rosariopfernandes/firepesa/Transaction.kt
+++ firepesa/src/main/java/io/github/rosariopfernandes/firepesa/Transaction.kt
@@ -1,38 +1,89 @@
+/*
+ *MIT License
+ *
+ *Copyright (c) 2018 Rosário Pereira Fernandes
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
 package io.github.rosariopfernandes.firepesa
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.HttpsCallableResult
 
+/**
+ * mPesa API Transaction
+ * @author Rosário Pereira Fernandes
+ */
 class Transaction {
     val functions = FirebaseFunctions.getInstance()
     private val payload : HashMap<String, Any> = HashMap()
 
+    /**
+     * Initiates a payment Transaction on the mPesa API
+     * @param msisdn Subscriber Number. Example: 841234567
+     * @param amount Amount being charged (currency:MZN)
+     * @param reference
+     * @param thirdPartyReference
+     * @return Task<HttpsCallableResult> from the Cloud Functions SDK
+     */
     fun payment(msisdn:String, amount:Float, reference:String,
                 thirdPartyReference:String): Task<HttpsCallableResult> {
-        payload.put("input_CustomerMSISDN", msisdn)
-        payload.put("input_Amount", "$amount")
-        payload.put("input_TransactionReference", reference)
-        payload.put("input_ThirdPartyReference", thirdPartyReference)
-        return functions.getHttpsCallable("payment")
+        payload.put("msisdn", "+258$msisdn")
+        payload.put("amount", "$amount")
+        payload.put("transactionReference", reference)
+        payload.put("thirdPartyReference", thirdPartyReference)
+        return functions.getHttpsCallable("testPayment")
                 .call(payload)
     }
 
+    /**
+     * Initiates a reversal (refund) Transaction on the mPesa API
+     * @param transactionId ID of the transaction to be reversed
+     * @param amount Amount being returned (currency:MZN)
+     * @return Task<HttpsCallableResult> from the Cloud Functions SDK
+     */
     fun refund(transactionId:String, amount:Float): Task<HttpsCallableResult> {
-        payload.put("input_Amount", "$amount")
-        payload.put("input_TransactionID", transactionId)
+        payload.put("transactionID", transactionId)
+        payload.put("amount", "$amount")
         return functions.getHttpsCallable("refund")
                 .call(payload)
     }
 
+    /**
+     * Initiates a Query Transaction on the mPesa API
+     * @param queryReference
+     * @return Task<HttpsCallableResult> from the Cloud Functions SDK
+     */
     fun query(queryReference:String): Task<HttpsCallableResult> {
         payload.put("input_QueryReference", queryReference)
         return functions.getHttpsCallable("query")
                 .call(payload)
     }
 
+    /**
+     * Replaces the default Notification with a custom one
+     * @param title Custom notification title
+     * @param body Custom notification body(content)
+     */
     fun addNotification(title:String, body:String){
         payload.put("fcmTitle", title)
-        payload.put("fcmMessage", body)
+        payload.put("fcmBody", body)
     }
 }
--- firepesa/src/main/java/io/github/rosariopfernandes/firepesa/ktx/Task.kt
+++ firepesa/src/main/java/io/github/rosariopfernandes/firepesa/ktx/Task.kt
@@ -1,3 +1,26 @@
+/*
+ *MIT License
+ *
+ *Copyright (c) 2018 Rosário Pereira Fernandes
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ */
 package io.github.rosariopfernandes.firepesa.ktx
 
 import com.google.android.gms.tasks.Task
@@ -5,24 +28,34 @@ import com.google.firebase.functions.HttpsCallableResult
 import io.github.rosariopfernandes.firepesa.TransactionResponse
 import java.lang.Exception
 
+/**
+ * This method will be called when the server replies to
+ * the sent Transaction
+ * @param response Response from the server
+ * @return Task<HttpsCallableResult> from the Cloud Functions SDK
+ */
 inline fun Task<HttpsCallableResult>.then(crossinline action:
        (response:TransactionResponse)->Unit):
         Task<HttpsCallableResult> {
     continueWith({
         task ->
-        var map = task.result.data as HashMap<String, String>
-        var response= TransactionResponse(
-                map["code"]!!,
-                map["description"]!!,
-                map["transactionId"]!!,
-                map["conversationId"]!!,
-                map["transactionStatus"]!!
+        val map = task.result.data as HashMap<String, String>
+        val response= TransactionResponse(
+                map["output_ResponseCode"]!!,
+                map["output_ResponseDesc"]!!,
+                map["output_TransactionID"]!!,
+                map["output_ConversationID"]!!,
+                map["output_ResponseTransactionStatus"]!!
         )
         action(response)
     })
     return this
 }
 
+/**
+ * This method will be called if an Exception was thrown on the server-side
+ * @param error Exception caught
+ */
 inline fun Task<HttpsCallableResult>.catch(crossinline action:
        (error:Exception)->Unit){
     addOnCompleteListener({
